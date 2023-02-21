9a92d420c96d006f007a73d0aa40aa17695cfa51
Sonu Gupta <rg3029926@gmail.com>
2023-02-22 03:38:02 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
-
 // import './App.css';
-import axios from "axios"
-import  React, { useState, useEffect } from 'react';
-import {Table, Container, Row, Col, Button, ButtonGroup, From, Navbar} from "react-bootstrap";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import {
+  Table,
+  Container,
+  Row,
+  Col,
+  Button,
+  ButtonGroup,
+  From,
+  Navbar,
+} from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const api="http://localhost:8080/posts"
+const api = "http://localhost:8080/posts";
 function App() {
-  const [user, setUser] = useState("")
+  const [user, setUser] = useState("");
 
   // const getdata= async () => {
   //   const res= await axios(api)
   //   const data= await res.json();
   //   console.log(data)
   // }
-  
+
   // useEffect(() => {
   // getdata()
   // }, [])
   return (
     <>
-      
-      <ToastContainer/>
-      <Navbar bg="yellow" variant='dark' className='justify-content-center'>
-        <Navbar.Brand>
-          Deploy Heroku
-        </Navbar.Brand>
-
+      <ToastContainer />
+      <Navbar bg="yellow" variant="dark" className="justify-content-center">
+        <Navbar.Brand>Deploy Heroku</Navbar.Brand>
       </Navbar>
-      <Container style={{marginTop:"70px"}}>
+      <Container style={{ marginTop: "70px" }}>
         <Row>
           <Col md={5}>
             <h2>From</h2>
